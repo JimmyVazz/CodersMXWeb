1438958294e6045ff8cc6f69bd5cf5d38fe09add
JimmyVazz <cjvasquez_98@hotmail.com>
2019-06-19 08:55:00 -0500

    Directorios listos

--- CodersMX/app.js
+++ CodersMX/app.js
@@ -10,14 +10,14 @@ const logger       = require('morgan');
 const path         = require('path');
 
 
-mongoose
+/*mongoose
   .connect('mongodb://localhost/codersmx', {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
     console.error('Error connecting to mongo', err)
-  });
+  });*/
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
