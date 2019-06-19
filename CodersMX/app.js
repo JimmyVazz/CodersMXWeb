f5f31d6c686134f54463b5a62fcc3b5e5601c816
JimmyVazz <cjvasquez_98@hotmail.com>
2019-06-19 17:04:15 -0500

    Login Design added

--- CodersMX/app.js
+++ CodersMX/app.js
@@ -9,15 +9,14 @@ const mongoose     = require('mongoose');
 const logger       = require('morgan');
 const path         = require('path');
 
-
-/*mongoose
-  .connect('mongodb://localhost/codersmx', {useNewUrlParser: true})
+mongoose
+  .connect(process.env.DB, {useNewUrlParser: true})
   .then(x => {
     console.log(`Connected to Mongo! Database name: "${x.connections[0].name}"`)
   })
   .catch(err => {
     console.error('Error connecting to mongo', err)
-  });*/
+  });
 
 const app_name = require('./package.json').name;
 const debug = require('debug')(`${app_name}:${path.basename(__filename).split('.')[0]}`);
@@ -43,12 +42,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(favicon(path.join(__dirname, 'public', 'images', 'favicon.ico')));
-
-
-
-// default value for title local
-app.locals.title = 'Express - Generated with IronGenerator';
-
 
 
 const index = require('./routes/index');
--- CodersMX/routes/index.js
+++ CodersMX/routes/index.js
@@ -6,4 +6,9 @@ router.get('/', (req, res, next) => {
   res.render('index');
 });
 
+/* Login de usuarios */
+router.get('/login', (req, res) =>{
+  res.render('login')
+})
+
 module.exports = router;
